e92d4ca2949730b61ae1cc8c864f6b6619744c4c
Jery <jery99961@gmail.com>
2024-11-16 18:30:57 +0530

    Disable hardcopy

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 const config = {
   API_BASE_URL: location.origin.replace(/:\d+/, ':5000') + '/api',
   SHOW_UPLOAD_ALERT: true,
-  HARDCOPY_DISABLED: false,
+  HARDCOPY_DISABLED: true,
 };
 
 export default config;
