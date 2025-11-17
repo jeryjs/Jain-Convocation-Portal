f6105bf4cad929a2151052f31eab360501fc9978
Student Affairs - FET, JU <studentaffairsfetju@gmail.com>
2025-11-17 23:31:54 +0530

    fix(package): specify port for start script to ensure correct server initialization

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
   API_BASE_URL: location.origin.replace(/:\d+/, ':5000') + '/api',
-  QUEUE_API_BASE_URL: location.origin.replace(/:\d+/, ':3000'),
+  QUEUE_API_BASE_URL: location.origin.replace(/:\d+/, ':4102'),
   SHOW_UPLOAD_ALERT: true,
   HARDCOPY_DISABLED: false,
   REMOVE_HARDCOPY: false,
