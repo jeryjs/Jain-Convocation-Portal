b65a57444ed52cb6ae3564f508da1252c043df94
Jery <jery99961@gmail.com>
2025-11-18 01:30:48 +0530

    fix(config): ensure correct QUEUE_API_BASE_URL for production environment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
   API_BASE_URL: location.origin.replace(/:\d+/, ':5000') + '/api',
-  QUEUE_API_BASE_URL: (process.env.NODE_ENV === 'development' ? location.origin.replace(/:\d+/, ':4102') : "face-search." + location.origin) + '/api',
+  QUEUE_API_BASE_URL: (process.env.NODE_ENV === 'development' ? location.origin.replace(/:\d+/, ':4102') : "https://face-search." + location.host) + '/api',
   SHOW_UPLOAD_ALERT: true,
   HARDCOPY_DISABLED: false,
   REMOVE_HARDCOPY: false,
